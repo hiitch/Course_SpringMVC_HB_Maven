7d28957b87952321a322befdc47b9ed50e419880
Alex Hien <alex.hien@raja.fr>
2022-05-27 13:08:25 +0200

    Migrate to full java config

---
+++ src/main/java/com/webcustomer/config/AppDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package com.webcustomer.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] { AppConfig.class };
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] { "/" };
+	}
+
+}
